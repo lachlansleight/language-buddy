b6f088f82ee1bb188ee6781328ede795420bd141
LachlanSleight <lachlansleight@gmail.com>
2024-02-04 08:30:35 +1100

    fix: Add additional copy plugin just for vercel

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ const nextConfig = {
           fs: false,
         };
     
+        //local dev server
         config.plugins.push(
           new CopyPlugin({
             patterns: [
@@ -51,6 +52,50 @@ const nextConfig = {
             ],
           })
         );
+
+        //vercel
+        config.plugins.push(
+            new CopyPlugin({
+              patterns: [
+                {
+                  from: "./node_modules/onnxruntime-web/dist/ort-wasm.wasm",
+                  to: "static/chunks",
+                },
+                {
+                  from: "./node_modules/onnxruntime-web/dist/ort-wasm-threaded.wasm",
+                  to: "static/chunks",
+                },
+                {
+                  from: "./node_modules/onnxruntime-web/dist/ort-wasm-simd.wasm",
+                  to: "static/chunks",
+                },
+                {
+                  from: "./node_modules/onnxruntime-web/dist/ort-wasm-simd.jsep.wasm",
+                  to: "static/chunks",
+                },
+                {
+                  from: "./node_modules/onnxruntime-web/dist/ort-wasm-simd-threaded.wasm",
+                  to: "static/chunks",
+                },
+                {
+                  from: "./node_modules/onnxruntime-web/dist/ort-wasm-simd-threaded.jsep.wasm",
+                  to: "static/chunks",
+                },
+                {
+                  from: "./node_modules/onnxruntime-web/dist/ort-training-wasm-simd.wasm",
+                  to: "static/chunks",
+                },
+                {
+                  from: "./node_modules/@ricky0123/vad-web/dist/silero_vad.onnx",
+                  to: "static/chunks",
+                },
+                {
+                  from: "./node_modules/@ricky0123/vad-web/dist/vad.worklet.bundle.min.js",
+                  to: "static/chunks",
+                },
+              ],
+            })
+          );
     
         return config;
       },
